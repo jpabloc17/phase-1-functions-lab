42ccd3cbc18e7e3a47510d8880d8c8415b88bd99
Pablo Castro <pablocas-tro1@outlook.com>
2023-03-20 15:45:09 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    return Math.abs(42 - location);
+}
+
+function distanceFromHqInFeet(blocks){
+    const numberOfBlocks = distanceFromHqInBlocks(blocks);
+    return numberOfBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distance = Math.abs(start - destination);
+    return distance * 264;
+}
+
+function calculatesFarePrice(start, destination){
+   const distance = distanceTravelledInFeet(start, destination);
+
+   if (distance <= 400){
+    return 0;
+   } else if (distance > 400 && distance < 2000){
+    return (distance - 400) * 0.02;
+   } else if (distance >= 2000 && distance < 2500){
+    return 25;
+   } else if (distance > 2500){
+    return "cannot travel that far";
+   }
+}
+
+console.log(distanceFromHqInBlocks(10));
+console.log(distanceFromHqInFeet(10));
+console.log(distanceTravelledInFeet(43,48));
+console.log(calculatesFarePrice(42,126));
